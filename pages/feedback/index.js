3bfa8e405e8af25ab6bd4e0720cecba2d9ff94b8
gingercook <vahitaras@gmail.com>
2022-09-29 20:04:23 -0400

    id.js done for api

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
+
 // Since this is a backend stuff, it will not be 
 // included in the client side bundle.
 import { getFeedbackPath, extractFeedback } from "../api";
 
 const FeedbackPage = (props) => {
-    console.log(props.data);
+
+    const [loadedEmail, setLoadedEmail] = useState();
+
+    const loadTextHandler = (id) => {
+        fetch(`/api/${id}`).then(res => res.json()).then(data => setLoadedEmail(data.requestedData.email));
+    };
 
     const allTextList = props.data.map(list =>
         <li key={list.id}>
-            {list.text} <button>Show Details</button>
+            {list.text}
+            <button onClick={loadTextHandler.bind(null, list.id)}>Show Details</button>
         </li>
     );
 
     return (
-        <ul>{allTextList}</ul>
+        <>
+            {loadedEmail && <p>{loadedEmail}</p>}
+            <ul>{allTextList}</ul>
+        </>
     );
 };
 
